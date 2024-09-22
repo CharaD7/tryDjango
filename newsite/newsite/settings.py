2a2a31b8aaf3d2f5ba81a2bbf0b381e890017563
CharaD7 <jijakahn6@gmail.com>
2024-09-22 08:08:31 +0000

    Deregistered unfold

--- newsite/newsite/settings.py
+++ newsite/newsite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'unfold.apps.BasicAppConfig',
+    # 'unfold.apps.BasicAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
