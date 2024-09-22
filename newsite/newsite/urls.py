75bdebcbc7a92f7cf86c6c2d542266b2b4140595
CharaD7 <jijakahn6@gmail.com>
2024-09-22 07:44:18 +0000

    Added new admin url path

--- newsite/newsite/urls.py
+++ newsite/newsite/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from unfold_ui.sites import new_admin_site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("unfold-admin/", new_admin_site.urls), # <-- Unfold admin
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
