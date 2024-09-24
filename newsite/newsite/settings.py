8ffdfe495ab9c2353718c344389aad4143cb04e7
CharaD7 <jijakahn6@gmail.com>
2024-09-24 18:40:34 +0000

    Installed customer config in settings

--- newsite/newsite/settings.py
+++ newsite/newsite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # 'unfold.apps.BasicAppConfig',
     'myapp.apps.MyappConfig',
+    'customer.apps.CustomerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
