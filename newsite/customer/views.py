917206f32dd91741697b2c2b70f1dc688363e272
CharaD7 <jijakahn6@gmail.com>
2024-09-24 21:39:15 +0000

    Created signup view

--- newsite/customer/views.py
+++ newsite/customer/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .forms import InputForm
+
 
 # Create your views here.
+
+
+def signup(request):
+    form = InputForm()
+    context = {"form": form}
+    return render(request, "signup.html", context)
