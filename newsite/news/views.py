fe8509a916bac6b179fe3a89abce3f28674ddea1
CharaD7 <jijakahn6@gmail.com>
2024-09-25 22:42:58 +0000

    Added Article vuew

--- newsite/news/views.py
+++ newsite/news/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Article
+
 
 # Create your views here.
-def news():
-    return HttpResponse("Here is the news page.")
+def year_archive(request, year):
+    a_list = Article.objects.filter(pub_date__year=year)
+    context = {"year": year, "article_list": a_list}
+    return render(request, "news/year_archive.html", context)
