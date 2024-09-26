a661b5d234bb2804e8872bf1604b9b58d88a1dc6
CharaD7 <jijakahn6@gmail.com>
2024-09-26 07:59:08 +0000

    Added form inputs for basic bio data in customer app

--- newsite/customer/forms.py
+++ newsite/customer/forms.py
@@ -8,8 +8,8 @@ SHIFTS = (
 )
 
 
-class InputForm(forms.form):
-    first_name = forms.CharField(maax_length=200)
-    last_name = forms.CharField(maax_length=200)
+class InputForm(forms.Form):
+    first_name = forms.CharField(max_length=200)
+    last_name = forms.CharField(max_length=200)
     shift = forms.ChoiceField(choices=SHIFTS)
     time_log = forms.TimeField()
