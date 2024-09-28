7927910c853ebeb4e7fd7ef4584124930c75879d
CharaD7 <jijakahn6@gmail.com>
2024-09-28 19:23:51 +0000

    Used render to simplify index view on polls

--- newsite/polls/views.py
+++ newsite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
@@ -7,11 +7,10 @@ from .models import Question
 # Create your views here.
 def index(request):
     last_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "last_question_list": last_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
